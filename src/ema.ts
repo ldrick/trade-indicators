810125ecb36c0570fa15aae79a01c4ae905a3d1f
ldrick <3674067+ldrick@users.noreply.github.com>
2020-10-15 21:51:43 +0200

    fix smma

--- src/ema.ts
+++ src/ema.ts
@@ -1,30 +1,4 @@
-import { avg } from './avg';
-import { NotEnoughDataError } from './NotEnoughDataError';
+import { dma } from './dma';
 
-export const ema = (values: number[], period = 20): number[] => {
-  const factor = 2 / (period + 1);
-  let previous: number;
-
-  if (values.length < period) {
-    throw new NotEnoughDataError();
-  }
-
-  return values.map((value, index, array) => {
-    const pointer = index + 1;
-
-    if (pointer < period) {
-      return 0;
-    }
-
-    let val = value;
-    if (pointer === period) {
-      // first is calculated as Moving Average
-      val = avg(array.slice(pointer - period, pointer));
-    } else {
-      val = (val - previous) * factor + previous;
-    }
-    previous = val;
-
-    return val;
-  });
-};
+export const ema = (values: number[], period = 20): number[] =>
+  dma(values, period, 2 / (period + 1));
