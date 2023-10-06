b9db74c5a75c9690b0d248d36b3c532554ae0376
Ricky Lippmann <ricky.lippmann@haufe-lexware.com>
2023-10-06 20:49:17 +0200

    upgrade dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,7 +12,7 @@ const config = {
 			files: ['*.ts'],
 			extends: [
 				'airbnb-typescript/base',
-				'plugin:@typescript-eslint/recommended-type-checked',
+				'plugin:@typescript-eslint/strict-type-checked',
 				'plugin:@typescript-eslint/stylistic-type-checked',
 				'plugin:jsdoc/recommended',
 				'plugin:eslint-comments/recommended',
