ceadc5cf66a20f35477a707c941c2702607ddb19
ldrick <3674067+ldrick@users.noreply.github.com>
2024-01-02 16:25:17 +0100

    update dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,15 +4,21 @@ import { defineConfig } from 'vitest/config';
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
 	test: {
+		globals: true,
 		include: ['./tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 		setupFiles: ['./tests/config/setup.ts'],
 		coverage: {
 			provider: 'v8',
-			branches: 100,
-			functions: 100,
-			lines: 100,
-			statements: 100,
+			include: ['src/**/*.ts'],
+			/**
+			 * Type-only or interface-only files need to be excluded,
+			 * as v8 collects them,	but they won't be covered.
+			 * @see https://github.com/vitest-dev/vitest/issues/3605
+			 */
+			exclude: ['src/types.ts'],
+			thresholds: {
+				100: true,
+			},
 		},
-		globals: false,
 	},
 });
