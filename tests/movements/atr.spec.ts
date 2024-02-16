adf51d1ee675ebea401fbbba54d3f026bdcb110b
ldrick <3674067+ldrick@users.noreply.github.com>
2024-02-16 12:20:27 +0100

    Eslint flat config (#14)
    * provide flat config
    
    * fix lint prettier and couple more
    
    ---------
    
    Co-authored-by: Ricky Lippmann <ricky.lippmann@haufe-lexware.com>

--- tests/movements/atr.spec.ts
+++ tests/movements/atr.spec.ts
@@ -1,5 +1,6 @@
 import { either as E } from 'fp-ts/lib';
 import { describe, expect, it } from 'vitest';
+
 import { NotEnoughDataError } from '../../src/errors/NotEnoughDataError.js';
 import { NotPositiveIntegerError } from '../../src/errors/NotPositiveIntegerError.js';
 import { UnequalArraySizesError } from '../../src/errors/UnequalArraySizesError.js';
@@ -7,14 +8,18 @@ import { atr } from '../../src/movements/atr.js';
 import * as prices from '../prices.json' assert { type: 'json' };
 
 describe('atr', () => {
-	it.each([{ p: NaN }, { p: Infinity }, { p: -Infinity }, { p: -1 }, { p: 0 }, { p: 1.5 }])(
-		'fails if period is not a positive integer $p',
-		({ p }) => {
-			expect(atr({ close: [1.3], high: [1.5], low: [0.9] }, p)).toStrictEqual(
-				E.left(new NotPositiveIntegerError()),
-			);
-		},
-	);
+	it.each([
+		{ p: Number.NaN },
+		{ p: Number.POSITIVE_INFINITY },
+		{ p: Number.NEGATIVE_INFINITY },
+		{ p: -1 },
+		{ p: 0 },
+		{ p: 1.5 },
+	])('fails if period is not a positive integer $p', ({ p }) => {
+		expect(atr({ close: [1.3], high: [1.5], low: [0.9] }, p)).toStrictEqual(
+			E.left(new NotPositiveIntegerError()),
+		);
+	});
 
 	it('fails if not enough data to calculate for period', () => {
 		expect(atr({ close: [1, 2, 3], high: [1, 2, 3], low: [1, 2, 3] }, 3)).toStrictEqual(
@@ -31,7 +36,7 @@ describe('atr', () => {
 	it.each([
 		{
 			v: {
-				high: [0, 0, NaN, 0],
+				high: [0, 0, Number.NaN, 0],
 				low: [0, 0, 0, 0],
 				close: [0, 0, 0, 0],
 			},
@@ -39,7 +44,7 @@ describe('atr', () => {
 		{
 			v: {
 				high: [0, 0, 0, 0],
-				low: [0, 0, Infinity, 0],
+				low: [0, 0, Number.POSITIVE_INFINITY, 0],
 				close: [0, 0, 0, 0],
 			},
 		},
@@ -47,7 +52,7 @@ describe('atr', () => {
 			v: {
 				high: [0, 0, 0, 0],
 				low: [0, 0, 0, 0],
-				close: [0, -Infinity, 0, 0],
+				close: [0, Number.NEGATIVE_INFINITY, 0, 0],
 			},
 		},
 	])('fails if any value is a infinit value $v', ({ v }) => {
