318490a656ac2703d007d156db6a3690a69dadcf
Ricky Lippmann <3674067+ldrick@users.noreply.github.com>
2024-08-04 12:50:08 +0200

    update dependencies

--- src/utils/toPromise.ts
+++ src/utils/toPromise.ts
@@ -4,7 +4,6 @@ import { either as E, function as F } from 'fp-ts/lib';
  * Provides a way to transform Either to Promise.
  * @public
  */
-// eslint-disable-next-line functional/prefer-immutable-types
 export const toPromise = <A>(values: E.Either<Error, A>): Promise<A> =>
 	F.pipe(
 		values,
