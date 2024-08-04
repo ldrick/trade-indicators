51d9b5aa981f5a691bdcd73d78d88111662e1bab
Ricky Lippmann <ricky.lippmann@gmail.com>
2024-08-04 12:50:08 +0200

    update dependencies

--- tests/typings/global.d.ts
+++ tests/typings/global.d.ts
@@ -6,6 +6,10 @@ type ArrayType<T> = Extract<
 
 declare global {
 	interface ArrayConstructor {
+		/**
+		 * Fixed type guard for arrays.
+		 * @see https://github.com/microsoft/TypeScript/issues/17002#issuecomment-1285847629
+		 */
 		isArray<T>(argument: T): argument is ArrayType<T>;
 	}
 }
