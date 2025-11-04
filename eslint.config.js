01d484ab11d29bfeb25a49cb4c18951e4d7c7cc1
Ricky Lippmann <ricky.lippmann@haufe-lexware.com>
2025-11-04 13:24:16 +0100

    add json and markdown linting with eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@
 
 import commentsPlugin from '@eslint-community/eslint-plugin-eslint-comments/configs';
 import eslint from '@eslint/js';
+import jsonPlugin from '@eslint/json';
+import markdownPlugin from '@eslint/markdown';
 import vitestPlugin from '@vitest/eslint-plugin';
 import configPrettier from 'eslint-config-prettier';
 import functionalPlugin from 'eslint-plugin-functional';
@@ -9,33 +11,64 @@ import importPlugin from 'eslint-plugin-import';
 import jsdocPlugin from 'eslint-plugin-jsdoc';
 import perfectionist from 'eslint-plugin-perfectionist';
 import unicornPlugin from 'eslint-plugin-unicorn';
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import typescriptEslint from 'typescript-eslint';
 
-export default typescriptEslint.config(
+const EXTENSIONS_MINIMATCH = {
+	JavaScript: ['**/*.cjs', '**/*.mjs', '**/*.js'],
+	JavaScriptReact: ['**/*.jsx'],
+	JSON: ['**/*.json'],
+	JSONC: ['**/*.jsonc'],
+	Markdown: ['**/*.md'],
+	TypeScript: ['**/*.cts', '**/*.mts', '**/*.ts'],
+	TypeScriptDefinition: ['**/*.d.ts'],
+	TypeScriptReact: ['**/*.tsx'],
+};
+
+export default defineConfig(
 	// register all of the plugins upfront
 	{
 		plugins: {
+			['@eslint-community/eslint-comments']:
+				commentsPlugin.recommended.plugins['@eslint-community/eslint-comments'],
 			['@typescript-eslint']: typescriptEslint.plugin,
+			// @ts-expect-error Plugin seems improperly typed
+			// @see https://github.com/eslint-functional/eslint-plugin-functional/issues/998
 			['functional']: functionalPlugin,
 			['import']: importPlugin,
 			['jsdoc']: jsdocPlugin,
+			['perfectionist']: perfectionist,
+			['unicorn']: unicornPlugin,
 			['vitest']: vitestPlugin,
 		},
 	},
 	// ignored files
 	{
-		ignores: ['node_modules', 'dist', 'coverage'],
+		ignores: ['node_modules', 'dist', 'coverage', 'package-lock.json'],
 	},
-	// extends
-	eslint.configs.recommended,
-	commentsPlugin.recommended,
-	...typescriptEslint.configs.strictTypeChecked,
-	...typescriptEslint.configs.stylisticTypeChecked,
-	unicornPlugin.configs['recommended'],
-	perfectionist.configs['recommended-natural'],
-	// base config
+	// restrict eslint recommended only to js and ts files
 	{
+		...eslint.configs.recommended,
+		files: [
+			...EXTENSIONS_MINIMATCH.JavaScript,
+			...EXTENSIONS_MINIMATCH.JavaScriptReact,
+			...EXTENSIONS_MINIMATCH.TypeScript,
+			...EXTENSIONS_MINIMATCH.TypeScriptReact,
+		],
+	},
+	// extend typescript-eslint recommended configs as in their docs
+	typescriptEslint.configs.strictTypeChecked,
+	typescriptEslint.configs.stylisticTypeChecked,
+	// base config for js and ts files
+	{
+		files: [
+			...EXTENSIONS_MINIMATCH.JavaScript,
+			...EXTENSIONS_MINIMATCH.JavaScriptReact,
+			...EXTENSIONS_MINIMATCH.TypeScript,
+			...EXTENSIONS_MINIMATCH.TypeScriptReact,
+			...EXTENSIONS_MINIMATCH.TypeScriptDefinition,
+		],
 		languageOptions: {
 			ecmaVersion: 2023,
 			globals: {
@@ -49,6 +82,9 @@ export default typescriptEslint.config(
 			},
 		},
 		rules: {
+			...commentsPlugin.recommended.rules,
+			...unicornPlugin.configs['recommended'].rules,
+			...perfectionist.configs['recommended-natural'].rules,
 			// disallow non-import statements appearing before import statements
 			'import/first': 'error',
 			// Require a newline after the last import/require in a group
@@ -85,16 +121,31 @@ export default typescriptEslint.config(
 			'unicorn/no-array-reduce': 'off',
 			'unicorn/no-null': 'off',
 		},
+		settings: {
+			'import/resolver': {
+				typescript: {
+					alwaysTryTypes: true,
+					project: import.meta.dirname,
+				},
+			},
+		},
 	},
-	// overrides for JavaScript files
+	// disable type-checked rules according to file types without typings
+	// https://typescript-eslint.io/troubleshooting/typed-linting/#how-can-i-disable-type-aware-linting-for-a-set-of-files
 	{
-		files: ['*.js'],
+		files: [
+			...EXTENSIONS_MINIMATCH.JavaScript,
+			...EXTENSIONS_MINIMATCH.JavaScriptReact,
+			...EXTENSIONS_MINIMATCH.JSON,
+			...EXTENSIONS_MINIMATCH.JSONC,
+			...EXTENSIONS_MINIMATCH.Markdown,
+		],
 		...typescriptEslint.configs.disableTypeChecked,
 	},
 	// overrides for TypeScript files
 	{
-		files: ['*.ts'],
-		ignores: ['**/*.d.ts'],
+		files: [...EXTENSIONS_MINIMATCH.TypeScript, ...EXTENSIONS_MINIMATCH.TypeScriptReact],
+		ignores: EXTENSIONS_MINIMATCH.TypeScriptDefinition,
 		rules: {
 			...jsdocPlugin.configs['flat/recommended-typescript-error'].rules,
 			'jsdoc/check-tag-names': [
@@ -132,7 +183,7 @@ export default typescriptEslint.config(
 	},
 	// overrides for TypeScript Definition files
 	{
-		files: ['**/*.d.ts'],
+		files: EXTENSIONS_MINIMATCH.TypeScriptDefinition,
 		rules: {
 			'@typescript-eslint/no-empty-object-type': [
 				'error',
@@ -145,14 +196,14 @@ export default typescriptEslint.config(
 	// overrides for functional TypeScript files
 	{
 		files: ['src/!(errors)/*.ts'],
-		ignores: ['**/*.d.ts'],
+		ignores: EXTENSIONS_MINIMATCH.TypeScriptDefinition,
 		...functionalPlugin.configs.externalTypescriptRecommended,
 		...functionalPlugin.configs.recommended,
 		...functionalPlugin.configs.stylistic,
 	},
 	// overrides for Test files
 	{
-		files: ['*.spec.ts'],
+		files: ['**/*.spec.ts'],
 		languageOptions: {
 			globals: {
 				...vitestPlugin.environments.env.globals,
@@ -172,6 +223,32 @@ export default typescriptEslint.config(
 			vitest: {
 				typecheck: true,
 			},
+		},
+	},
+	// overrides for json files
+	{
+		extends: ['json/recommended'],
+		files: EXTENSIONS_MINIMATCH.JSON,
+		language: 'json/json',
+		plugins: {
+			['json']: jsonPlugin,
+		},
+	},
+	// overrides for jsonc files
+	{
+		extends: ['json/recommended'],
+		files: [...EXTENSIONS_MINIMATCH.JSONC, '**/tsconfig.json', '.vscode/*.json'],
+		language: 'json/jsonc',
+		plugins: {
+			['json']: jsonPlugin,
+		},
+	},
+	// overrides for markdown files
+	{
+		extends: ['markdown/recommended'],
+		files: EXTENSIONS_MINIMATCH.Markdown,
+		plugins: {
+			['markdown']: markdownPlugin,
 		},
 	},
 	// overrides for Config files
