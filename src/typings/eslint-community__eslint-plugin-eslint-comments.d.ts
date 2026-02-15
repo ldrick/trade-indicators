ddfdc03e399b015e49dc3dbd589de94ec2e4f1c4
Ricky Lippmann <ricky.lippmann@gmail.com>
2026-02-15 15:53:53 +0100

    update dependencies

--- src/typings/eslint-community__eslint-plugin-eslint-comments.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@eslint-community/eslint-plugin-eslint-comments/configs' {
-	import type { Linter } from 'eslint';
-
-	const recommended: Linter.Config & {
-		// @see https://github.com/eslint-community/eslint-plugin-eslint-comments/issues/215
-		plugins: NonNullable<Linter.Config['plugins']>;
-	};
-	export = { recommended };
-}
