c1853514e71d0a0cadb36640b1ee5e1c815a539e
Ricky Lippmann <ricky.lippmann@haufe-lexware.com>
2026-03-14 16:54:18 +0100

    upgrade dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,24 @@
 // @ts-check
 
-import commentsPlugin from '@eslint-community/eslint-plugin-eslint-comments';
+import commentsPlugin, {
+	configs as commentsConfigs,
+} from '@eslint-community/eslint-plugin-eslint-comments';
 import eslint from '@eslint/js';
 import jsonPlugin from '@eslint/json';
 import markdownPlugin from '@eslint/markdown';
 import vitestPlugin from '@vitest/eslint-plugin';
 import configPrettier from 'eslint-config-prettier';
 import functionalPlugin from 'eslint-plugin-functional';
-import importPlugin from 'eslint-plugin-import';
+import { importX as importXPlugin } from 'eslint-plugin-import-x';
 import jsdocPlugin from 'eslint-plugin-jsdoc';
-import perfectionist from 'eslint-plugin-perfectionist';
+import { configs as perfectionistConfigs } from 'eslint-plugin-perfectionist';
 import unicornPlugin from 'eslint-plugin-unicorn';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
-import typescriptEslint from 'typescript-eslint';
+import {
+	configs as typescriptEslintConfigs,
+	plugin as typescriptEslintPlugin,
+} from 'typescript-eslint';
 
 const EXTENSIONS_MINIMATCH = {
 	JavaScript: ['**/*.cjs', '**/*.mjs', '**/*.js'],
@@ -31,13 +36,10 @@ export default defineConfig(
 	{
 		plugins: {
 			['@eslint-community/eslint-comments']: commentsPlugin,
-			['@typescript-eslint']: typescriptEslint.plugin,
-			// @ts-expect-error Plugin seems improperly typed
-			// @see https://github.com/eslint-functional/eslint-plugin-functional/issues/998
+			['@typescript-eslint']: typescriptEslintPlugin,
 			['functional']: functionalPlugin,
-			['import']: importPlugin,
+			['import-x']: importXPlugin,
 			['jsdoc']: jsdocPlugin,
-			['perfectionist']: perfectionist,
 			['unicorn']: unicornPlugin,
 			['vitest']: vitestPlugin,
 		},
@@ -48,7 +50,7 @@ export default defineConfig(
 	},
 	// restrict eslint recommended only to js and ts files
 	{
-		...eslint.configs.recommended,
+		extends: [eslint.configs.recommended],
 		files: [
 			...EXTENSIONS_MINIMATCH.JavaScript,
 			...EXTENSIONS_MINIMATCH.JavaScriptReact,
@@ -57,10 +59,16 @@ export default defineConfig(
 		],
 	},
 	// extend typescript-eslint recommended configs as in their docs
-	typescriptEslint.configs.strictTypeChecked,
-	typescriptEslint.configs.stylisticTypeChecked,
+	typescriptEslintConfigs.strictTypeChecked,
+	typescriptEslintConfigs.stylisticTypeChecked,
 	// base config for js and ts files
 	{
+		extends: [
+			importXPlugin.configs['flat/recommended'],
+			importXPlugin.configs['flat/typescript'],
+			unicornPlugin.configs.recommended,
+			perfectionistConfigs['recommended-natural'],
+		],
 		files: [
 			...EXTENSIONS_MINIMATCH.JavaScript,
 			...EXTENSIONS_MINIMATCH.JavaScriptReact,
@@ -81,57 +89,18 @@ export default defineConfig(
 			},
 		},
 		rules: {
-			...commentsPlugin.configs.recommended.rules,
-			...unicornPlugin.configs['recommended'].rules,
-			...perfectionist.configs['recommended-natural'].rules,
-			// disallow non-import statements appearing before import statements
-			'import/first': 'error',
-			// Require a newline after the last import/require in a group
-			'import/newline-after-import': 'error',
-			// Forbid import of modules using absolute paths
-			'import/no-absolute-path': 'error',
-			// disallow AMD require/define
-			'import/no-amd': 'error',
-			// forbid default exports - we want to standardize on named exports so that imported names are consistent
-			'import/no-default-export': 'error',
-			// disallow imports from duplicate paths
-			'import/no-duplicates': 'error',
-			// Forbid the use of extraneous packages
-			'import/no-extraneous-dependencies': [
-				'error',
-				{
-					devDependencies: true,
-					optionalDependencies: false,
-					peerDependencies: true,
-				},
-			],
-			// Forbid mutable exports
-			'import/no-mutable-exports': 'error',
-			// Prevent importing the default as if it were named
-			'import/no-named-default': 'error',
-			// Prohibit named exports
-			'import/no-named-export': 'off', // we want everything to be a named export
-			// Forbid a module from importing itself
-			'import/no-self-import': 'error',
-			// Require modules with a single export to use a default export
-			'import/prefer-default-export': 'off', // we want everything to be named
+			...commentsConfigs.recommended.rules,
+			'import-x/no-default-export': 'error',
 			'unicorn/filename-case': ['error', { cases: { camelCase: true, pascalCase: true } }],
 			'unicorn/no-array-callback-reference': 'off',
 			'unicorn/no-array-reduce': 'off',
 			'unicorn/no-null': 'off',
 		},
-		settings: {
-			'import/resolver': {
-				typescript: {
-					alwaysTryTypes: true,
-					project: import.meta.dirname,
-				},
-			},
-		},
 	},
 	// disable type-checked rules according to file types without typings
 	// https://typescript-eslint.io/troubleshooting/typed-linting/#how-can-i-disable-type-aware-linting-for-a-set-of-files
 	{
+		extends: [typescriptEslintConfigs.disableTypeChecked],
 		files: [
 			...EXTENSIONS_MINIMATCH.JavaScript,
 			...EXTENSIONS_MINIMATCH.JavaScriptReact,
@@ -139,14 +108,13 @@ export default defineConfig(
 			...EXTENSIONS_MINIMATCH.JSONC,
 			...EXTENSIONS_MINIMATCH.Markdown,
 		],
-		...typescriptEslint.configs.disableTypeChecked,
 	},
 	// overrides for TypeScript files
 	{
+		extends: [jsdocPlugin.configs['flat/recommended-typescript-error']],
 		files: [...EXTENSIONS_MINIMATCH.TypeScript, ...EXTENSIONS_MINIMATCH.TypeScriptReact],
 		ignores: EXTENSIONS_MINIMATCH.TypeScriptDefinition,
 		rules: {
-			...jsdocPlugin.configs['flat/recommended-typescript-error'].rules,
 			'jsdoc/check-tag-names': [
 				'error',
 				{
@@ -188,20 +156,39 @@ export default defineConfig(
 				'error',
 				{ allowInterfaces: 'with-single-extends', allowObjectTypes: 'never' },
 			],
-			'import/no-default-export': 'off',
+			'import-x/no-default-export': 'off',
 			'unicorn/filename-case': 'off',
 		},
 	},
 	// overrides for functional TypeScript files
 	{
+		extends: [
+			functionalPlugin.configs.recommended,
+			functionalPlugin.configs.externalTypeScriptRecommended,
+			functionalPlugin.configs.stylistic,
+		],
 		files: ['src/!(errors)/*.ts'],
 		ignores: EXTENSIONS_MINIMATCH.TypeScriptDefinition,
-		...functionalPlugin.configs.externalTypescriptRecommended,
-		...functionalPlugin.configs.recommended,
-		...functionalPlugin.configs.stylistic,
+		settings: {
+			immutability: {
+				overrides: [
+					{
+						// Always treat big.js's Big as Immutable.
+						to: 'Immutable',
+						type: { from: 'package', package: 'big.js', pattern: /^Big.BigConstructor.Big$/ },
+					},
+					{
+						// Always treat TypeScript's ReadonlyArrays as Immutable.
+						to: 'Immutable',
+						type: { from: 'lib', name: 'ReadonlyArray' },
+					},
+				],
+			},
+		},
 	},
 	// overrides for Test files
 	{
+		extends: [vitestPlugin.configs.recommended],
 		files: ['**/*.spec.ts'],
 		languageOptions: {
 			globals: {
@@ -209,7 +196,6 @@ export default defineConfig(
 			},
 		},
 		rules: {
-			...vitestPlugin.configs.recommended.rules,
 			'vitest/consistent-test-it': [
 				'error',
 				{
@@ -226,7 +212,7 @@ export default defineConfig(
 	},
 	// overrides for json files
 	{
-		extends: ['json/recommended'],
+		extends: [jsonPlugin.configs.recommended],
 		files: EXTENSIONS_MINIMATCH.JSON,
 		language: 'json/json',
 		plugins: {
@@ -235,7 +221,7 @@ export default defineConfig(
 	},
 	// overrides for jsonc files
 	{
-		extends: ['json/recommended'],
+		extends: [jsonPlugin.configs.recommended],
 		files: [...EXTENSIONS_MINIMATCH.JSONC, '**/tsconfig.json', '.vscode/*.json'],
 		language: 'json/jsonc',
 		plugins: {
@@ -244,9 +230,10 @@ export default defineConfig(
 	},
 	// overrides for markdown files
 	{
-		extends: ['markdown/recommended'],
+		extends: [markdownPlugin.configs.recommended],
 		files: EXTENSIONS_MINIMATCH.Markdown,
 		plugins: {
+			// @ts-expect-error - markdown plugin currently not supporting eslint@10
 			['markdown']: markdownPlugin,
 		},
 	},
@@ -254,7 +241,7 @@ export default defineConfig(
 	{
 		files: ['eslint.config.js', 'vitest.config.ts'],
 		rules: {
-			'import/no-default-export': 'off',
+			'import-x/no-default-export': 'off',
 		},
 	},
 	// prettier has to be the last extension
